68dc28e187179a8c36067a12e08bdfb2fd323029
lharagon <loowieg@gmail.com>
2019-02-19 12:32:35 -0800

    Complete basic registration form, add extender validation

--- RegistrationForm/WebContent/javascript/registrationform.js
+++ RegistrationForm/WebContent/javascript/registrationform.js
@@ -1,13 +1,54 @@
 /* Module for Registration form application */
 var RegistrationForm = function () {
+
+    ko.extenders.required = function(target, option) {
+        target.hasError = ko.observable(false);
+
+        target.subscribe(function(newValue) {
+            target.hasError(newValue ? false : true);
+        });
+
+        return target;
+    }
+
     /* add members here */
     var customer = {
         personalInfo: {
             title: ko.observable(),
-            firstName: ko.observable(),
+            firstName: ko.observable().extend({ required: null }),
             middleName: ko.observable(),
             lastName: ko.observable()
-        }
+        },
+        contactDetails: {
+            phoneNumber: ko.observable(),
+            emailAddress: ko.observable(),
+            preferredContact: ko.observable()
+        },
+        address: {
+            residential: {
+                street: ko.observable(),
+                city: ko.observable(),
+                postCode: ko.observable(),
+                country: ko.observable()
+            },
+            postal: {
+                type: ko.observable(),
+                streetAddress: {
+                    street: ko.observable(),
+                    city: ko.observable(),
+                    postCode: ko.observable(),
+                    country: ko.observable()
+                },
+                poBoxAddress: {
+                    poBox: ko.observable(),
+                    city: ko.observable(),
+                    postCode: ko.observable(),
+                    country: ko.observable()
+                }
+            }
+        },
+        creditCards: ko.observableArray(),
+        interests: ko.observableArray(),
     }
 
     /* options for the title drop down*/
@@ -34,9 +75,18 @@ var RegistrationForm = function () {
       }
     ];
 
+    var addCreditCard = function() {
+        customer.creditCards.push({name: ko.observable(), number: ko.observable(), expiryDate: ko.observable()});
+    };
+
+    var deleteCreditCard = function(card) {
+        console.log("Deleting credit card with number: ", + card.number());
+        customer.creditCards.remove(card);
+    }
 
     var init = function () {
         /* add code to initialize this module */
+        addCreditCard();
         ko.applyBindings(RegistrationForm);
     };
 
@@ -46,6 +96,26 @@ var RegistrationForm = function () {
         console.log(ko.toJSON(customer));
     };
 
+    var clear = function() {
+        console.log("Clear customer model");
+        traverseAndClearModel(customer);
+        addCreditCard();
+    }
+
+    var traverseAndClearModel = function(jsonObj) {
+        $.each(jsonObj, function(key, val) {
+            if(ko.isObservable(val)) {
+                if(val.removeAll != undefined) {
+                    val.removeAll();
+                } else {
+                    val(null);
+                }
+            } else {
+                traverseAndClearModel(val);
+            }
+        })
+    }
+
     var titleSelect = ko.pureComputed(function() {
         if (customer.personalInfo.title() == null) {
             console.log("Hello")
@@ -53,8 +123,7 @@ var RegistrationForm = function () {
         } else {
             return customer.personalInfo.title();
         }
-    })
-
+    });
 
     /* execute the init function when the DOM is ready */
     $(init);
@@ -64,6 +133,8 @@ var RegistrationForm = function () {
         customer: customer,
         submit: submit,
         titleOptions: titleOptions,
-        titleSelect: titleSelect
+        titleSelect: titleSelect,
+        addCreditCard: addCreditCard,
+        clear: clear,
     };
 }();
